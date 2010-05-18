8fc047d4d7d25b26668a8d21286dc2700777d800
cedwards <cedwards@daphne.part.net>
2010-05-18 16:28:48 -0600

    added full path to bstack and bstackhoriz patches

--- config.h
+++ config.h
@@ -33,8 +33,8 @@ static const Rule rules[] = {
 static const float mfact      = 0.55; /* factor of master area size [0.05..0.95] */
 static const Bool resizehints = False; /* True means respect size hints in tiled resizals */
 
-#include "bstack.c"
-#include "bstackhoriz.c"
+#include "/home/cedwards/Projects/dwm/bstack.c"
+#include "/home/cedwards/Projects/dwm/bstackhoriz.c"
 
 static const Layout layouts[] = {
 	/* symbol   arrange function */
