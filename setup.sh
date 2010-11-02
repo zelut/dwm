51133915e26d6dee23d6fc551cb6e5fd03833d3c
Christer Edwards <christer.edwards@gmail.com>
2010-11-02 13:08:13 -0600

    terminus font (vs droid), smarter .xinitrc and setup.sh

--- setup.sh
+++ setup.sh
@@ -19,6 +19,7 @@ fi
 (pacman -Q rxvt-unicode-256color)   || exit 1
 (pacman -Q terminus-font)           || exit 1
 (pacman -Q slim)                    || exit 1
+(pacman -Q feh)			    || exit 1
 
 # begin linking
 ln -s "${DWM_GIT}"/.Xdefaults          "${DWM_DEST}"/.Xdefaults &>/dev/null
@@ -27,14 +28,18 @@ ln -s "${DWM_GIT}"/battery.py          "${DWM_DEST}"/battery.py &>/dev/null
 ln -s "${DWM_GIT}"/server_room_temp.pl "${DWM_DEST}"/server_room_temp.pl &>/dev/null
 ln -s "${DWM_GIT}"/dwm.png             "${DWM_DEST}"/dwm.png
 
-/usr/bin/feh --bg-scale "${DWM_GIT}"/dwm.png
-
 /bin/cp -a dwm02 /usr/share/slim/themes/
 
 /bin/cp -a 20-fonts.conf /etc/X11/xorg.conf.d/
 
-wget -q -c http://aur.archlinux.org/packages/urxvt-url-select/urxvt-url-select.tar.gz -O ${DWM_DEST}/Downloads/
+wget -q -c http://aur.archlinux.org/packages/urxvt-url-select/urxvt-url-select.tar.gz -O ${DWM_DEST}/Downloads/urxvt-url-select.tar.gz
+wget -q -c http://aur.archlinux.org/packages/ttf-droid-monovar/ttf-droid-monovar.tar.gz -O ${DWM_DEST}/Downloads/ttf-droid-monovar.tar.gz
+wget -q -c http://aur.archlinux.org/packages/gtk-theme-shiki-colors-murrine/gtk-theme-shiki-colors-murrine.tar.gz -O ${DWM_DEST}/Downloads/gtk-theme-shiki-colors-murrine.tar.gz
+wget -q -c http://aur.archlinux.org/packages/gnome-colors-icon-theme/gnome-colors-icon-theme.tar.gz -O ${DWM_DEST}/Downloads/gnome-colors-icon-theme.tar.gz
 
 echo
 echo "==> Build the 'urxvt-url-select' package in ~/Downloads"
+echo "==> Build the 'ttf-droid-monovar' package in ~/Downloads"
+echo "==> Build the 'gtk-theme-shiki-colors-murrine' package in ~/Downloads"
+echo "==> Build the 'gnome-colors-icon-theme' package in ~/Downloads"
 echo
